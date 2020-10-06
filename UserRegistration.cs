ac0f89ba8a7ed90e3a577768213608f1aa65e4b7
Vishal Garg <vishugarg10004@gmail.com>
2020-10-06 21:19:14 +0530

    [Vishal] added 4th UC of password

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -14,22 +14,27 @@ namespace UserRegistrationProblem
         }
         public bool Email(string input)
         {
-            string emailPattern = @"^[a-z]{1}[a-z0-9]*([._]?[a-z0-9]+)*\@[a-z0-9]+\.(com|net)(\.[a-z]{2})?$";
+            //string emailPattern = @"^[a-z]{1}[a-z0-9]*([._]?[a-z0-9]+)*\@[a-z0-9]+\.(com|net)(\.[a-z]{2})?$";
+              string emailPattern = @"^([a-z0-9A-Z])+([._+-]?[a-z0-9A-Z]+)*[@]{1}[a-z0-9A-Z]+[.]{1}[a-zA-Z]{3}([.]{1}[a-z]{2})?$";
             return Regex.IsMatch(input, emailPattern);
         }
         public bool MobileNo(string input)
         {
-            string mobileNoPattern = @"\d{2}\s{1}[6-9]{1}\d{9}";
+            //string mobileNoPattern = @"\d{2}\s{1}[6-9]{1}\d{9}";
+            string mobileNoPattern = @"^[1-9]{2}[ ]?[0-9]{10}$";
             return Regex.IsMatch(input, mobileNoPattern);
         }
         public bool Password(string input)
         {
-            //string passwordPattern = "^([A-Z]+[a-z0-9]*){8,}";
-            string passwordPattern = @"^(?=.*[A-Z])(?=.*[0-9])[a-zA-Z\d]{8,}$";
+            //string passwordPattern = "^[A-Z]{1}[a-z0-9]{8,}";
+            //string passwordPattern = @"^(?=.*[A-Z])(?=.*[0-9])(([a-zA-Z\d]*)\@([a-zA-Z\d]+)){8,}$";
+             string passwordPattern = @"^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&*])([a-zA-Z\d@#$%^&*]){8,}$";
+            //string passwordPattern= "^(([a-z0-9]*)[A-Z]{1,}([a-z0-9A-Z]+)){8,}";
             //https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
             //https://stackoverflow.com/questions/21197394/what-does-do-in-regex/21197429
             //https://stackoverflow.com/questions/10804732/what-is-the-difference-between-and-in-regex
             return Regex.IsMatch(input, passwordPattern);
+            //Vishal@1111
         }
     }
 }
