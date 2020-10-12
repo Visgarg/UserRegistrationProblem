83a9a4b9d27c5b803071e773acf4949aa679f289
Vishal Garg <vishugarg10004@gmail.com>
2020-10-12 13:47:00 +0530

    [Vishal] added mstest methods for validating user name, email, mobile and passeword.

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -4,37 +4,39 @@ using System.Text.RegularExpressions;
 
 namespace UserRegistrationProblem
 {
-    class UserRegistration
+    public class UserRegistration
     {
+        /// <summary>
+        /// takes pattern for first name and last name.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
         public bool FirstAndLastName(string input)
         {
+            //pattern for first name and last name
             string firstNamePattern = "[A-Z]{1}[A-Za-z]{2,}";
             return Regex.IsMatch(input, firstNamePattern);
 
         }
         public bool Email(string input)
         {
+            //pattern for mail id.
             //string emailPattern = @"^[a-z]{1}[a-z0-9]*([._]?[a-z0-9]+)*\@[a-z0-9]+\.(com|net)(\.[a-z]{2})?$";
               string emailPattern = @"^([a-z0-9A-Z])+([._+-]?[a-z0-9A-Z]+)*[@]{1}[a-z0-9A-Z]+[.]{1}[a-zA-Z]{3}([.]{1}[a-z]{2})?$";
+            //satisfies all sample cases of mails.
             return Regex.IsMatch(input, emailPattern);
         }
         public bool MobileNo(string input)
         {
-            //string mobileNoPattern = @"\d{2}\s{1}[6-9]{1}\d{9}";
-            string mobileNoPattern = @"^[1-9]{2}[ ]?[0-9]{10}$";
+            //pattern for phone no.
+            string mobileNoPattern = @"\d{2}\s{1}[6-9]{1}\d{9}";
+            //string mobileNoPattern = @"^[1-9]{2}[ ]{1}[0-9]{10}$";
             return Regex.IsMatch(input, mobileNoPattern);
         }
         public bool Password(string input)
         {
-            //string passwordPattern = "^[A-Z]{1}[a-z0-9]{8,}";
-            //string passwordPattern = @"^(?=.*[A-Z])(?=.*[0-9])(([a-zA-Z\d]*)\@([a-zA-Z\d]+)){8,}$";
-             string passwordPattern = @"^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&*])([a-zA-Z\d@#$%^&*]){8,}$";
-            //string passwordPattern= "^(([a-z0-9]*)[A-Z]{1,}([a-z0-9A-Z]+)){8,}";
-            //https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
-            //https://stackoverflow.com/questions/21197394/what-does-do-in-regex/21197429
-            //https://stackoverflow.com/questions/10804732/what-is-the-difference-between-and-in-regex
+            string passwordPattern = @"^(?=.*[A-Z])(?=.*[0-9])(?=.*[^0-9a-zA-Z])(?!.*[^0-9a-zA-Z].*[^0-9a-zA-Z]).{8,}$";
             return Regex.IsMatch(input, passwordPattern);
-            //Vishal@1111
         }
     }
 }
